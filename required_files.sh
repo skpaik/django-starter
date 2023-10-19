88c7f1e9bb709091faa61bd44bb552cab3d97405
spaik <spaik@tbscg.com>
2023-10-19 16:36:42 +0600

    # Feature - update_files.sh updated

--- required_files.sh
+++ required_files.sh
@@ -2,10 +2,15 @@
 
 touch Dockerfile
 touch docker-compose.yml
+touch .dockerignore
+
 touch .env
 touch .env.sample
+
 touch README.md
 touch .gitignore
+
 touch requirements.txt
+
 touch build.sh
 touch run.sh
